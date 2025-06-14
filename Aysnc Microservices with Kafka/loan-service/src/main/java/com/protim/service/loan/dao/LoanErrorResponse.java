466d8d4f37df298f45b60d65971a16295e43c77e
vishwaprotim <vishwaprotimbanerjee93@gmail.com>
2025-06-14 20:13:44 +0530

    Commit - Initial Commit

--- Aysnc Microservices with Kafka/loan-service/src/main/java/com/protim/service/loan/dao/LoanErrorResponse.java
+++
@@ -1,5 +0,0 @@
-package com.protim.service.loan.dao;
-
-import org.springframework.http.HttpStatus;
-
-public record LoanErrorResponse(HttpStatus status, String message) {}
--- Aysnc Microservices with Kafka/loan-service/src/main/java/com/protim/service/loan/dao/LoanResponse.java
+++
@@ -1,9 +0,0 @@
-package com.protim.service.loan.dao;
-
-
-import com.protim.service.loan.entity.UserLoan;
-import org.springframework.http.HttpStatus;
-
-public record LoanResponse(HttpStatus status,
-                           String message,
-                           UserLoan userLoan) {}
